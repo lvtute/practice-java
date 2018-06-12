086f76213e9d30cd8c853e60eb40f6148aff1076
Thuan <lvtute@gmail.com>
2018-06-12 21:03:32 +0700

    first commit

---
+++ hello world/src/FirstSample.java
@@ -0,0 +1,16 @@
+public class FirstSample
+{
+	public static void main(String[] args)
+	{
+		System.out.println("We will not use 'Hello, World!'");
+		int[] a = new int[10];
+		for (int i : new int[] { 17, 19, 23, 29, 31, 37 }
+
+
+) {
+			System.out.println(i);
+		}
+	}
+}
+
+
